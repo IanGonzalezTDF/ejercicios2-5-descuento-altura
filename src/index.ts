ffa746514f4260be87946a970fa468852d618d78
IanGonzalezTDF <102770923+IanGonzalezTDF@users.noreply.github.com>
2022-05-06 20:01:29 -0300

    ejercicio-altura

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,13 @@
-rotulo1.innerHTML = "Ingresar monto";
-rotulo2.innerHTML = "Ingresar cantidad";
+rotulo1.innerHTML = "Ingresar altura";
 
 let boton = document.getElementById("enviar");
 boton.addEventListener("click", () => {
-  let monto: number = dato1.value;
-  let cantidad: number = dato2.value;
-  let montoConDescuento: number = 0;
-  let descuento: number = 0;
-  let precioTotal: number = 0;
-  precioTotal = monto * cantidad;
+  let alturaPermitida: number = 1.3;
+  let alturaPersona: number = dato.value;
 
-  if (precioTotal >= 1000) {
-    descuento = (precioTotal * 10) / 100;
-    montoConDescuento = precioTotal - descuento;
-    console.log(
-      "Por gastar mas de 1000 obtiene un descuento del %10 descuento"
-    );
-    console.log("El monto a pagar es:" + montoConDescuento);
+  if (alturaPersona <= alturaPermitida) {
+    console.log("La persona NO puede pasar");
   } else {
-    console.log("No tiene descuento. El monto a pagar es:" + precioTotal);
+    console.log("La persona puede pasar");
   }
 });
